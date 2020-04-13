156cee0262c81f7234275ea676cea5384e53870d
Diego Garcia <61706873+dgnorieg@users.noreply.github.com>
2020-04-13 16:09:55 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,37 @@
+
+## Crea un objeto matriz especial que puede almacenar en caché su inverso
 makeCacheMatriz <- function(x = matriz()) {
-inv <- NULL
-set <- function(y) {
-x <<- y
+# Define la función para establecer el valor de la matriz
+# inverso del caché
+	inv <- NULL
+	set <- function(y) {
+	x <<- y
+	inv <<- NULL
 }
+# Define la función para obtener el valor de la matriz
 get <- function() x
-setinversa <- function(inverse) inv <<- inversa
-getinversa <- function() inv
-list(set = set,
-get = get,
-setinversa = setinversa,
-getinversa = getinversa)
+# Define la función para establecer el inverso.
+# no hay inverso en caché
+	setinversa <- function(inverse) inv <<- inversa
+# Define la función para obtener el inverso
+	getinversa <- function() inv
+# Devuelve una lista con las cuatro funciones anteriores
+	list(set = set, get = get,
+	setinversa = setinversa,
+	getinversa = getinversa)
 }
 
-cachesolucion <- function(x, ...) {
-inv <- x$getinversa()
-if (!is.null(inv)) {
-message("datos en cache")
-return(inv)
+
+cacheSolve <- function(x, ...) {
+	inv <- x$getinversa() # Esto recupera el valor almacenado en caché para el inverso
+	if (!is.null(inv)) { # Si el caché no estaba vacío, podemos devolverlo
+	message("datos en cache")
+	return(inv)
 }
-mat <- x$get()
-inv <- solve(mat, ...)
-x$setinversa(inv)
-inv
+
+# El caché estaba vacío. Necesitamos calcularlo, almacenarlo en caché y luego devolverlo.
+mat <- x$get() # Obtener valor de matriz
+	inv <- solve(mat, ...) # se calcula la inversa
+	x$setinversa(inv) # resultado en cache
+	inv # Regresa el inverso
 }
